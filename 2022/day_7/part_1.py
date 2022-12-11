a437ae26bb42dae3340991b9e7478c16ea9e1222
Alexander Tabakov <alexander.tabakov@auto1.com>
2022-12-11 12:23:40 +0100

    day 7, 2022

---
+++ 2022/day_7/part_1.py
@@ -0,0 +1,55 @@
+import os
+
+from collections import deque
+from collections import defaultdict
+
+## read input
+result = 0
+limit = 100000
+dirs = defaultdict()
+path = []
+
+def process_cmd(line: str):
+    global path
+
+    if line[2:4] == 'ls':
+        return
+    
+    _, _, dir = line.split(' ')
+    
+    if dir == '..':
+        path.pop()
+    else:
+        path.append(dir)
+
+def process_file(line: str):
+    global dirs, path
+    
+    l, _ = line.split(' ')
+
+    if l == 'dir':
+        return
+    
+    curr = ''
+    for dir in path:
+        curr = curr + '.' + dir
+        dirs[curr] = dirs.get(curr, 0) + int(l)
+
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
+
+with open(os.path.join(__location__, 'input'), "r") as file:
+    for line in file:
+        line = line.rstrip("\n")
+        if line[0] == '$':
+            process_cmd(line)
+        else:
+            process_file(line)
+
+for size in dirs.values():
+    if size <= limit:
+        result = result + size
+
+print(result)
