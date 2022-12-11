0827a3dd324d7b3df2fcfdb05a44df7575acda5d
Alexander Tabakov <alexander.tabakov@auto1.com>
2022-12-11 11:24:25 +0100

    day 6, 2022

---
+++ 2022/day_6/part_2.py
@@ -0,0 +1,23 @@
+import os
+
+from collections import deque
+
+## read input
+result = 0
+q = deque()
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
+
+with open(os.path.join(__location__, 'input'), "r") as file:
+    for line in file:
+        for i in range(len(line)):
+            q.append(line[i])
+            if len(q) == 14:
+                if len(set(q)) == 14:
+                    print(i + 1)
+                    exit
+                else:
+                    q.popleft()
+        
